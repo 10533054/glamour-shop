e4296fccc048de4cfa9e026fe67581003ddb913c
10533054 <10533054@mydbs.ie>
2020-03-03 02:33:38 +0530

    create-user UI

--- client/glamour-shop/src/app/app.module.ts
+++ client/glamour-shop/src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule, MatButtonModule} from '@angular/material';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CreateUserComponent } from './create-user/create-user.component';
+import { MatOptionModule } from '@angular/material/core';
 
 @NgModule({
   declarations: [
@@ -13,6 +16,11 @@ import { CreateUserComponent } from './create-user/create-user.component';
   ],
   imports: [
     BrowserModule,
+    MatFormFieldModule,
+    MatOptionModule,
+    MatInputModule,
+    MatButtonModule,
+    
     AppRoutingModule,
     BrowserAnimationsModule
   ],
