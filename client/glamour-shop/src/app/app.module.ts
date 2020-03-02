6621a125be7e0bb06c2f1644c6d199101f3fd6cf
10533054 <10533054@mydbs.ie>
2020-03-03 03:01:04 +0530

    sidenav

--- client/glamour-shop/src/app/app.module.ts
+++ client/glamour-shop/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule, MatButtonModule, MatToolbarModule} from '@angular/material';
+import {MatInputModule, MatButtonModule, MatToolbarModule, MatIconModule, MatSidenavModule} from '@angular/material';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -25,6 +25,8 @@ import { ItemsListComponent } from './items-list/items-list.component';
     MatInputModule,
     MatButtonModule,
     MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
     
     AppRoutingModule,
     BrowserAnimationsModule
